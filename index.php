834e38278f45f62bb8424c5cd26ab8d7c7befed4
Kusanagi8200 <elijahkaminski230@gmail.com>
2024-05-05 22:21:31 +0000

    Update

--- index.php
+++ index.php
@@ -6,72 +6,95 @@
     <style>
         .button {
             display: block;
-            width: 40%; /* Ajustez la largeur si nécessaire */
+            width: 40%;
             padding: 10px;
-            margin-bottom: 10px; /* Espace entre les boutons */
-            margin-left: auto; /* Centrer le bouton horizontalement */
-            margin-right: auto; /* Centrer le bouton horizontalement */
-            background-color: #E65100; /* Couleur de fond */
-            color: black; /* Couleur du texte */
+            margin-bottom: 10px;
+            margin-left: auto;
+            margin-right: auto;
+            background-color: #E65100;
+            color: black;
             text-align: center;
             font-size: 16px;
-            font-weight: bold; /* Rendre le texte en gras */
+            font-weight: bold;
             border: none;
             border-radius: 5px;
             cursor: pointer;
         }
         .button:hover {
-            background-color: #45a049; /* Couleur de fond au survol */
+            background-color: #45a049;
         }
-        /* Centrer le conteneur de boutons pour un alignement esthétique */
         .button-container {
-            text-align: center; /* Aligner les boutons centrés dans leur conteneur */
-            width: 100%; /* Prend la largeur complète du conteneur parent pour une meilleure gestion de l'espace */
+            text-align: center;
+            width: 100%;
         }
-        /* Style pour le bandeau en haut de la page */
+
+        .bottom-banner {
+            background-color: orange;
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            color: black;
+            text-align: center;
+            padding: 10px 0;
+            font-size: 15px;
+            width: 100%;
+            border-radius: 10px; /* Coins arrondis ajoutés ici */
+        }
+
+        .bottom-banner a {
+            color: inherit;
+            text-decoration: none;
+        }
+
+        .bottom-banner a:hover {
+            text-decoration: underline;
+        }
+
+        .highlight {
+            background-color: orange;
+            color: black;
+            padding: 5px;
+            text-align: center;
+        }
+
+        .ascii-art {
+            font-family: 'Courier New', monospace;
+            white-space: pre;
+            text-align: left;
+            color: black;
+            background-color: orange;
+            border-radius: 10px; /* Vous pouvez également ajouter des coins arrondis ici si désiré */
+            margin: 20px 0;
+        }
         .top-banner {
-            background-color: orange; /* Couleur de fond orange */
-            color: black; /* Texte en noir */
-            text-align: center; /* Centrer le texte */
-            padding: 10px 0; /* Padding vertical pour un peu d'espace */
-            font-size: 25px; /* Taille de la police pour rendre le texte visible */
-            width: 100%; /* S'étend sur toute la largeur */
-        }
-
-	.bottom-banner {
-    	background-color: orange; /* Couleur de fond orange */
-	position: fixed; /* Fixe le bandeau au bas de la fenêtre */
-    	bottom: 0; /* Positionne le bandeau tout en bas */
-    	left: 0; /* Align�gauche */
-    	color: black; /* Texte en noir */
+    	background-color: #E65100; /* Couleur d'arrière-plan */
+    	color: black; /* Couleur du texte */
     	text-align: center; /* Centrer le texte */
-    	padding: 10px 0; /* Padding vertical pour un peu d'espace */
-    	font-size: 15px; /* Taille de la police pour rendre le texte visible */
-    	width: 100%; /* S'étend sur toute la largeur */
+    	padding: 10px 0; /* Espacement intérieur */
+    	font-size: 20px; /* Taille du texte */
+    	border-radius: 10px; /* Coins arrondis */
+    	margin-bottom: 20px; /* Espace en dessousxy du bandeau */
 	}
-	
-	.bottom-banner a {
-    	color: inherit; /* Herite la couleur du parent, ici noir */
-    	text-decoration: none; /* Supprime le soulignement par defaut des liens */
-	}
-
-	.bottom-banner a:hover {
-    	text-decoration: underline; /* Ajoute un soulignement au survol pour indiquer qu'il s'agit d'un lien */
-	}
-
-	.highlight {
-    	background-color: orange; /* Couleur de fond orange */
-    	color: black;           /* Texte en noir */
-    	padding: 5px;           /* Ajoute un peu de padding autour du texte */
-	text-align: center;       /* Centre le texte horizontalement */
-	}
-
     </style>
+
 </head>
 <body>
-    <!-- Bandeau en haut de la page -->
-    <div class="top-banner"><strong>KUSAPP / 2024 - UPDATE INFOS APPLICATION</strong></div>
-         <h2 class="highlight"<strong>MAINTENANCE SCRIPT FOR LINUX SYSTEM</strong></h2>
+    <pre class="ascii-art">
+
+╔╗╔═╗─────╔═══╗───────────────────────────────────────────────────────╔═══╦═══╦═══╦╗─╔╗
+║║║╔╝─────║╔═╗║───────────────────────────────────────────────────────║╔═╗║╔═╗║╔═╗║║─║║
+║╚╝╝╔╗╔╦══╣║─║╠══╦══╗─────────────────────────────────────────────────╚╝╔╝║║║║╠╝╔╝║╚═╝║
+║╔╗║║║║║══╣╚═╝║╔╗║╔╗║╔══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╦══╗╔═╝╔╣║║║╠═╝╔╩══╗║
+║║║╚╣╚╝╠══║╔═╗║╚╝║╚╝║╚══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╩══╝║║╚═╣╚═╝║║╚═╗──║║
+╚╝╚═╩══╩══╩╝─╚╣╔═╣╔═╝─────────────────────────────────────────────────╚═══╩═══╩═══╝──╚╝
+──────────────║║─║║
+──────────────╚╝─╚╝
+ </pre>
+
+    <div class="top-banner">
+    <strong>MAINTENANCE SCRIPT LINUX SYSTEM</strong>
+    </div>
+
     <div class="button-container">
         <!-- Chaque bouton dans son propre formulaire pointant vers un script PHP spécifique -->
 	<form action="test-shell.php" method="post"><button class="button" type="submit">TEST</button></form>
