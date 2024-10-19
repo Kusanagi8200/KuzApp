529804b3aefac2a1c7203d2ef124a4d6501a4f13
Kusanagi8200 <elijahkaminski230@gmail.com>
2024-10-19 13:49:24 -0400

    Update

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'admin1');
-define('DB_PASSWORD', 'Kusanagi2045');
-define('DB_NAME', 'registration');
+define('DB_SERVER', 'db'); // Nom du service MySQL dans docker-compose.yml
+define('DB_USERNAME', 'admin1'); // Utilisateur MySQL
+define('DB_PASSWORD', 'kusanagi2045'); // Mot de passe MySQL (vérifiez si c'est vide dans votre configuration)
+define('DB_NAME', 'registration'); // Nom de la base de données
 
 // Connexion à la base de données MySQL
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
