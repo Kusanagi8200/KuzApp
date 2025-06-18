b73e0fb3bf5c8a343e60cc0c459678d3a91eb7f5
vkz@8200e/k-S9CD <116643083+Kusanagi8200@users.noreply.github.com>
2025-06-18 11:22:21 +0200

    Update update-script.eng.php

--- update-script.eng.php
+++ update-script.eng.php
@@ -1,254 +1,77 @@
-#!/bin/bash
+<?php
+// Spécifiez le chemin vers votre script shell
+$command = 'bash /var/www/html/KuzApp/kuzapp-script/update-script.eng.sh';
+ob_start();
+passthru($command);
+$output = ob_get_clean();
 
-# This program is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// Ajout d'en-têtes pour le contenu HTML
+header('Content-Type: text/html; charset=utf-8');
 
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// Affichage du résultat avec style de terminal
+echo "<!DOCTYPE html>";
+echo "<html><head>";
+echo "<style>";
+echo "body, html {";
+echo "    height: 100%;"; // Donne une hauteur de 100% au corps et au HTML
+echo "    margin: 0;"; // Supprime les marges par défaut
+echo "    padding: 0;"; // Supprime les paddings par défaut
+echo "    display: flex;";
+echo "    flex-direction: column;";
+echo "    background-color: #000;"; // Fond noir du reste de la page
+echo "}";
 
-echo #
-echo  "\033[43;30m LINUX SYSTEM CLEANUP AND UPDATE SCRIPT / https://github.com/Kusanagi8200 \033[0m"
+echo ".banner {";
+echo "    width: 100%;"; // Largeur totale
+echo "    background-color: orange;"; // Couleur de fond
+echo "    color: black;"; // Couleur de texte
+echo "    text-align: center;"; // Texte centré
+echo "    padding: 10px 0;"; // Padding vertical
+echo "    font-size: 20px;"; // Taille de police
+echo "    font-weight: bold;"; // Gras
+echo "    border-radius: 5px;"; // Arrondir tous les coins
+echo "    margin-bottom: 20px;"; // Espace entre la bannière et le contenu
+echo "}";
 
-if [ `whoami` != "root" ]
-then
-        echo  "\033[5;41;30m WARNING. YOU MUST HAVE SUDO RIGHTS TO RUN THIS SCRIPT \033[0m"
-        exit 1
-fi
+echo ".terminal {";
+echo "    width: 65%;";
+echo "    background-color: #ffa500;";
+echo "    color: #000;";
+echo "    font-family: monospace;";
+echo "    padding: 20px;";
+echo "    white-space: pre-wrap;";
+echo "    overflow: auto;"; // Ajoute une barre de défilement si nécessaire
+echo "    margin: 20px auto;";
+echo "    height: 600px;"; // Hauteur fixe pour le terminal
+echo "    border-radius: 5px;"; // Arrondir les coins de la zone terminale
+echo "    font-weight: bold;"; // Texte du résultat en gras
+echo "}";
 
-#WARNING UPDATE
-echo  "\033[5;41;37m WARNING: AN UPDATE CAN DAMAGE YOUR SYSTEM \033[0m"
-confirm()
-{
-    read -r -p "${1} [y/N] " response
+echo ".bottom-banner {";
+echo "    position: fixed;";
+echo "    bottom: 0;";
+echo "    left: 0;";
+echo "    width: 100%;";
+echo "    background-color: orange;";
+echo "    color: black;";
+echo "    text-align: center;";
+echo "    padding: 10px 0;";
+echo "    font-size: 20px;";
+echo "    font-weight: bold;";
+echo "    border-radius: 5px;"; // Arrondir tous les coins
+echo "}";
 
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
-}
+echo "a.bottom-link {";
+echo "    color: black;"; // Couleur du texte
+echo "    text-decoration: none;"; // Aucune décoration de texte
+echo "    display: block;"; // Assure que le lien prend toute la largeur du bandeau
+echo "}";
 
-if confirm "DO YOU WANT TO CONTINUE ?"; then
-   echo #
-   echo  "\033[44;37m ---> UPDATING IN PROGRESS <--- \033[0m"
-else
-    echo #
-    echo  "\033[44;37m ---> UPDATE COMPLETE <--- \033[0m"
-    exit
-fi
-
-echo #
-echo  "\033[43;30m ---> CHECK LOG FILES \033[0m"
-
-echo #
-if [  /var/log/update_upgrade.log ]
-then
-    echo  "\033[47;32m FILE \033[0m" /var/log/update_upgrade.log = "\033[47;32m OK \033[0m"
-else
-    echo  "\033[47;31m FILE DOESN'T EXIST \033[0m" & touch /var/log/update_upgrade.log
-    echo  "\033[41;33m --> FILE CREATION \033[0m" /var/log/update_upgrade.log = "\033[47;32m DONE \033[0m"
-fi
-
-echo #
-if [ -e /var/log/update_upgrade.err ]
-then
-    echo  "\033[47;32m FILE \033[0m" /var/log/update_upgrade.err = "\033[47;32m OK \033[0m"
-else
-    echo  "\033[47;31m FILE DOESN'T EXIST \033[0m" & touch /var/log/update_upgrade.err
-    echo  "\033[41;33m --> FILE CREATION \033[0m" /var/log/update_upgrade.err = "\033[47;32m DONE \033[0m"
-fi
-echo #
-
-echo #
-echo  "\033[43;30m ---> PRE-UPDATE CLEANING \033[0m"
-echo #
-echo  "\033[44;37m APT CLEAN \033[0m"
-apt clean 
-echo Done
-echo #
-echo  "\033[44;37m APT AUTOCLEAN \033[0m"
-apt autoclean
-echo #
-echo  "\033[44;37m APT REMOVE \033[0m"
-apt remove 
-echo #
-echo  "\033[44;37m APT AUTOREMOVE \033[0m"
-apt autoremove
-echo #
-echo  "\033[44;37m APT PURGE \033[0m"
-apt purge
-echo #
-echo  "\033[44;37m APT CHECK \033[0m"
-apt-get check
-echo #
-echo  "\033[43;30m <--- END OF PRE-UPDATE CLEANING \033[0m"
-
-echo #
-echo # 
-
-echo  "\033[43;30m ---> UPDATING PACKAGES \033[0m"
-apt update && apt list --upgradable
-echo #
-
-confirm()
-{
-    read -r -p "${1} [y/N] " response
-
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
-}
-
-if confirm "UPGRADE ?"; then
-   apt upgrade && apt --fix-broken install
-else
-    echo #
-    echo  "\033[44;30m ---> NO UPGRADE <--- \033[0m"
-    echo #
-    echo  "\033[44;30m ---> END OF SCRIPT <--- \033[0m"
-    exit
-fi
-
-echo #
-echo # 
-
-echo  "\033[43;30m <--- PACKAGE UPDATE COMPLETE \033[0m"
-echo #
-echo # 
-
-echo  "\033[43;30m ---> POST-UPDATE CLEANUP \033[0m"
-echo #
-echo  "\033[44;37m APT CLEAN \033[0m"
-apt clean 
-echo Done
-echo #
-echo  "\033[44;37m APT AUTOCLEAN \033[0m"
-apt autoclean
-echo #
-echo  "\033[44;37m APT REMOVE \033[0m"
-apt remove 
-echo #
-echo  "\033[44;37m APT AUTOREMOVE \033[0m"
-apt autoremove
-echo #
-echo  "\033[44;37m APT PURGE \033[0m"
-apt purge
-echo #
-echo  "\033[44;37m APT CHECK \033[0m"
-apt-get check
-echo #
-
-echo  "\033[43;30m ---> CACHE CLEANING \033[0m"
-sync; echo 3 > /proc/sys/vm/drop_caches
-echo  "\033[44;37m DONE \033[0m"
-echo # 
-
-echo  "\033[43;30m ---> BIN CLEANING \033[0m"
-rm -r -f ~/.local/share/Trash/files/*
-echo  "\033[44;37m DONE \033[0m"
-echo #
-
-echo  "\033[43;30m ---> CLEANING PACKAGE CONFIGURATIONS \033[0m"
-if [ "$(dpkg -l | grep ^rc)" ]; then
-     dpkg -P $(dpkg -l | awk '/^rc/{print $2}')
-else
-    echo "\033[44;37m NO PACKAGES TO BE PURGED \033[0m"
-fi
-echo #
-
-echo  "\033[43;30m <--- END OF POST-UPDATE CLEANUP \033[0m"
-echo #
-echo #
-
-echo  "\033[43;30m ---> SYSTEME INFORMATION <--- \033[0m"
-cat /proc/version
-echo #
-
-cat /etc/os-release
-echo #
-echo #
-
-echo  "\033[43;30m KERNEL LIST \033[0m"
-dpkg -l | grep -Ei "linux-(g|h|i|lo|si|t)" |sort -k3 | cut -d" " -s -f1,2,3 | column -s" " -t
-echo #
-echo #
-
-echo  "\033[43;30m FREE DISK SPACE \033[0m"
-df -h 
-echo #
-echo #
-
-echo  "\033[43;30m HARDWARE AND USER INFORMATION \033[0m"
-
-#INSTALLATION DE NEOFETCH CHECK INFOS SYSTEM
-confirm()
-{
-    read -r -p "${1} [y/N] " response
-
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
-}
-
-if confirm "INSTALL NEOFETCH ?"; then
-   apt install neofetch && neofetch
-else
-    echo #
-    echo  "\033[44;37m ---> NEOFETCH WILL NOT BE INSTALLED <--- \033[0m"
-    
-fi
-echo #
-
-echo # 
-echo  "\033[43;30m ---> NO PACKAGES TO BE PURGED \033[0m"
-
-if [ -e /var/log/update_upgrade.err ] && [ /var/log/update_upgrade.err -nt /var/log/update_upgrade.err ]
-then
-    echo  "\033[5;41;37m WARNING \033[0m"
-    cat /var/log/update_upgrade.err
-    echo
-else
-    echo  "\033[44;37m NO UPDATE ERROR \033[0m"
-fi
-echo #
-echo #
-
-echo #
-echo #
-
-# Reboot ? Function
-confirm()
-{
-    read -r -p "${1} [y/N] " response
-
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            true
-            ;;
-        *)
-            false
-            ;;
-    esac
-}
-
-if confirm "REBOOT ?"; then
-   reboot
-else
-    echo #
-    echo  "\033[44;37m ---> END OF SCRIPT <--- \033[0m"
-    
-fi
-echo #
+echo "</style>";
+echo "</head><body>";
+echo "<div class='banner'>KuzApp Project - Version Beta 0.1</div>";
+echo "<div class='terminal'><pre>$output</pre></div>";
+// Modification ici pour faire du texte dans le bandeau inférieur un lien de retour
+echo "<div class='bottom-banner'><a href='app.php' class='bottom-link'>RETOUR</a></div>";
+echo "</body></html>";
+?>
